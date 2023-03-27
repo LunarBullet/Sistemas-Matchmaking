5c60fd924efe3e6c9c9d499a76574efe26258f7b
Ravenoid01 <juanda.rave147@gmail.com>
2023-03-27 12:17:40 -0500

    Inicio de sesion y registro

--- Assets/Scripts/AutentificacionUsuario.cs
+++ Assets/Scripts/AutentificacionUsuario.cs
@@ -7,56 +7,132 @@ using Proyecto26;
 
 public class AutentificacionUsuario : MonoBehaviour
 {
-    public TextMeshProUGUI userNameText;
-    public TMP_InputField inputName;
+    private TextMeshProUGUI userNameText;
+    private TMP_InputField inputName;
 
-    public string playerName;
+    public TMP_InputField inputEmailLI;
+    public TMP_InputField inputPasswordLI;
+
+    public TMP_InputField inputEmailSU;
+    public TMP_InputField inputNameSU;
+    public TMP_InputField inputPasswordSU;
+
+
+    public static string playerName;
     public User user;
 
-    // Start is called before the first frame update
+    private string idToken;
+    public static string localId;
+
+    private string databaseURL = "https://snakeio-17b24-default-rtdb.firebaseio.com/users/";
+    private string WebAPIKey = "AIzaSyB0Ez-Ht5kc-FTei8YEg89aHjbLFHf7IZE";
+
     void Start()
     {
 
     }
 
-    public void Ingresar()
+    // Esta parte solo es para testear que sí este recibiendo los datos de firebase
+    /*public void IngresarNombreUsuario()
     {
-        playerName = inputName.text;
         PostToDatabase();
     }
-
     public void GetUserData()
     {
-        // Esta parte solo es para testear que sí este recibiendo los datos de firebase
         RetriveFromeDatabase();
     }
     public void UpdateData()
     {
         userNameText.text = "Nombre: " + user.userName;
     }
-
-    private void PostToDatabase()
-    {
-        User user = new User(playerName);
-        RestClient.Put("https://snakeio-17b24-default-rtdb.firebaseio.com/"+ playerName+ ".json", user);
-    }
     private void RetriveFromeDatabase()
     {
-        RestClient.Get<User>("https://snakeio-17b24-default-rtdb.firebaseio.com/" + inputName.text + ".json").Then(response =>
+        RestClient.Get<User>(databaseURL + inputName.text + ".json").Then(response =>
         {
             user = response;
             UpdateData();
         });
+    }*/
+
+    private void PostToDatabase()
+    {
+        User user = new User();
+        RestClient.Put(databaseURL+ localId+ ".json", user);
     }
+    public void SignUpButton()
+    {
+        SignUp(inputEmailSU.text, inputNameSU.text, inputPasswordSU.text);
+    }
+    public void LogInButton()
+    {
+        LogIn(inputEmailLI.text, inputPasswordLI.text);
+    }
+    private void SignUp(string email, string username, string password)
+    {
+        string userData = "{\"email\":\"" + email + "\",\"password\":\"" + password + "\",\"returnSecureToken\":true}";
+        RestClient.Post<SignUpResponse>("https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=" + WebAPIKey, userData).Then(
+            response =>
+            {
+                idToken = response.idToken;
+                localId = response.localId;
+                playerName = username;
+                PostToDatabase();
+
+            }).Catch(error =>
+            {
+                Debug.Log(error);
+            });
+        
+    }
+    private void LogIn(string email, string password)
+    {
+        string userData = "{\"email\":\"" + email + "\",\"password\":\"" + password + "\",\"returnSecureToken\":true}";
+        RestClient.Post<SignUpResponse>("https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=" + WebAPIKey, userData).Then(response =>
+        {
+            idToken = response.idToken;
+            localId = response.localId;
+            GetUsername();
+
+        }).Catch(error =>
+        {
+            Debug.Log(error);
+        });
+    }
+    private void GetUsername()
+    {
+        RestClient.Get<User>(databaseURL + localId + ".json").Then(response =>
+        {
+            playerName = response.userName;
+            Debug.Log(playerName + " iniciaste sesion");
+
+        }).Catch(error =>
+        {
+            Debug.Log(error);
+        });
+    }
+    
 
 }
 [System.Serializable] 
 public class User
 {
     public string userName;
-    
+    public string localId;
+
+    public User()
+    {
+        userName = AutentificacionUsuario.playerName;
+        localId = AutentificacionUsuario.localId;
+    }
     public User(string userName)
     {
         this.userName = userName; 
     }
+}
+[System.Serializable]
+public class SignUpResponse
+{
+    public string localId;
+    public string idToken;
+
 }
